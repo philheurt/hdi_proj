c2576fe2d522494bfa6e445db1f193a9cd7e8849
bibiteix <bibi.teix@hotmail.com>
2018-05-25 00:20:01 -0300

    Attempt to stick the side bar of design2

---
+++ js/fixedSideBar.js
@@ -0,0 +1,16 @@
+// // When the user scrolls down 20px from the top of the document, slide down the navbar
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.getElementById("side_nav").style.top = "0";
+//     } else {
+//         document.getElementById("side_nav").style.top = "-50px";
+//     }
+// }
+
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         console.log("TRIGGER");
+//     });
+// });
